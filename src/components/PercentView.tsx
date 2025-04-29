961cbd56c9174ed2e47b2236919a7d2102b38632
vojjtech <vojtech.platek.023@pslib.cz>
2025-04-29 22:12:22 +0200

    styl styl

--- src/components/PercentView.tsx
+++ src/components/PercentView.tsx
@@ -16,8 +16,7 @@ const PercentView: FC<PercentViewType> = ({ max, value, /*makeColor*/ }) => {
     const [currentMax, setCurrentMax] = useState(max)
 
     const percent = (currentValue / currentMax) * 100
-    const percentFloored = Math.round(percent * 10) / 10
-    const percentLimited = Math.max(0, Math.min(percentFloored, 100))
+    const percentLimited = Math.max(0, Math.min(Math.round(percent * 10) / 10, 100))
 
     const hue = percentLimited / 100 * 120
 
@@ -27,23 +26,25 @@ const PercentView: FC<PercentViewType> = ({ max, value, /*makeColor*/ }) => {
             <div className={style.percentview}>
 
                 <label className={style.percentviewinputs}>
-                        <div className={style.input}>
-                            <span>value:</span>
-                            <input type='number?' value={currentValue} onChange={(x) => setCurrentValue(Number(x.target.value))} />
-                        </div>
-    
-                        <div className={style.input}>
-                            <span>max:</span>
-                            <input type='number?' value={currentMax} onChange={(x) => setCurrentMax(Number(x.target.value))} />
+                        <div className={style.valueinputs}>
+                            <div className={style.input}>
+                                <span>Value:</span>
+                                <input type='number?' value={currentValue} onChange={(x) => setCurrentValue(Number(x.target.value))} />
+                            </div>
+        
+                            <div className={style.input}>
+                                <span>Max:</span>
+                                <input type='number?' value={currentMax} onChange={(x) => setCurrentMax(Number(x.target.value))} />
+                            </div>
                         </div>
 
-                    <input type='range' value={percent} onChange={(x) => setCurrentValue(Number(x.target.value) * currentMax / 100)} />
                 </label>
 
                 <span>{percentLimited}%</span>
                 <div className={style.percentviewbar}>
                     <div className={style.percentviewbarprogress} style={{ backgroundColor: `hsl(${hue}, 100%,50%)` /*makeColor(value/max)*/, width: `${percentLimited}%` }}></div>
                 </div>
+                <input type='range' step={0.001} value={percent} onChange={(x) => setCurrentValue(Math.round(Number(x.target.value)/100 * currentMax))} />
             </div>
         </>
     )
